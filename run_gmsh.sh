800a745290f7358aad6fba0b0e43688017fd40a6
Daniel Elias Diamon Vazquez <danielpdiamon@gmail.com>
2026-06-22 07:52:38 -0400

    Delete run_gmsh.sh

--- run_gmsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script envoltorio para ejecutar gmsh con la ruta de librerías correcta
-LIB_DIR="$(pwd)/fake_root_o_g/data/data/com.diamon.civil/files/usr/lib"
-GMSH_BIN="$(pwd)/fake_root_o_g/data/data/com.diamon.civil/files/usr/bin/gmsh"
-
-export LD_LIBRARY_PATH="$LIB_DIR:$LD_LIBRARY_PATH"
-
-"$GMSH_BIN" "$@"
